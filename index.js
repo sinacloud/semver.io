527f971986c5508d03f9d0196ad3e2c12da0b4c9
selboo <xuguocan@gmail.com>
2019-12-06 18:15:24 +0800

    使用国内镜像

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ if (process.env.NODE_ENV === 'production') require('newrelic');
 var log = require('./lib/logger');
 var App = require('./lib/app');
 
-var PORT = process.env.PORT || 5000;
+var PORT = process.env.PORT || 5050;
 
 var app = new App();
 
